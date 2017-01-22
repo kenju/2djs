db6cba506118a79f6b6d4eb8f1e7a9ab39ba927e
Kenju Wagatsuma <ken901waga@gmail.com>
2017-01-22 11:48:02 +0900

    addRow and addColumn

--- src/index.js
+++ src/index.js
@@ -3,9 +3,34 @@ export default class TwoDimensionalArray {
     this.arr = items || [[]];
   }
 
+  stats() {
+    let maxColumnSize = 0;
+    this.arr.forEach((row) => {
+      if (row.length > maxColumnSize) {
+        maxColumnSize = row.length;
+      }
+    });
+    const output = `
+    rows   : ${this.arr.length}
+    columns: ${maxColumnSize}
+    `;
+    console.log(output);
+  }
+
   printPretty() {
     this.arr.forEach((row) => {
       console.log(row);
+    });
+  }
+
+  addRow(items) {
+    this.arr.push(items);
+  }
+
+  addColumn(items) {
+    items.forEach((item, index) => {
+      const row = this.arr[index];
+      row.push(item);
     });
   }
 }
--- test/index.spec.js
+++ test/index.spec.js
@@ -1,9 +1,31 @@
 import TwoDimensionalArray from '../src/index';
 
-const items = [
-  [1, 2, 3, 4],
-  [5, 6, 7, 8],
-  [9, 10, 11, 12],
-];
-const array = new TwoDimensionalArray(items);
-array.printPretty();
+describe('TwoDimensionalArray', () => {
+  const items = [
+    [1, 2, 3, 4],
+    [5, 6, 7, 8],
+    [9, 10, 11, 12],
+  ];
+  const array = new TwoDimensionalArray(items);
+  describe('#stats', () => {
+    it('print outs array stats', () => {
+      array.stats();
+    });
+  });
+
+  describe('#addRow', () => {
+    it('add row', () => {
+      const row = [13, 14, 15, 16];
+      array.addRow(row);
+      array.printPretty();
+    });
+  });
+
+  describe('#addColumn', () => {
+    it('add column', () => {
+      const column = [0, 0, 0, 0];
+      array.addColumn(column);
+      array.printPretty();
+    });
+  });
+});
